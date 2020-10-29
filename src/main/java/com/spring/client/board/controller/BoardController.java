ace9030a331ff54afad98cc029dcb1657791a981
khotaehyeong <irasklira_@naver.com>
2020-10-29 09:27:37 +0900

    20.10.29

--- src/main/java/com/spring/client/board/controller/BoardController.java
+++ src/main/java/com/spring/client/board/controller/BoardController.java
@@ -1,4 +1,15 @@
 package com.spring.client.board.controller;
 
+import com.spring.client.board.service.BoardService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
 public class BoardController {
+    private BoardService boardService;
 }
--- src/main/java/com/spring/client/board/service/BoardServiceImpl.java
+++ src/main/java/com/spring/client/board/service/BoardServiceImpl.java
@@ -1,4 +1,12 @@
 package com.spring.client.board.service;
 
+import com.spring.client.board.dao.BoardDao;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardServiceImpl {
+    @Setter(onMethod_=@Autowired)
+    private BoardDao boardDao;
 }
