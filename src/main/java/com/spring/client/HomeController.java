d6b5e2092e5e9513984abe610240e2cd6ac13152
hanbo <hanbo@DESKTOP-9IMDPRA>
2020-10-31 15:46:50 +0900

    얘가 자꾸 화면만져요

--- src/main/java/com/spring/client/HomeController.java
+++ src/main/java/com/spring/client/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	/* 얘가 자꾸 화면 만져요 */
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
